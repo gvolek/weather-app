89241c1dac8e0fea7ddc7103803de0cdd4d6b3c6
Gema R Volek <gemarubycastro@gmail.com>
2021-10-05 11:15:58 -0600

    Added Real Code to JS file

--- app.js
+++ app.js
@@ -1,1 +1,61 @@
+function showTemperature(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#CurrentTemp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+}
+function EnterCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-city-input").value;
+  let apiKey = "8b2194a8687d081654f1ef7f23c1a526";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showTemperature);
+}
+let searchForm = document.querySelector("#search-engine");
+searchForm.addEventListener("submit", EnterCity);
 
+function findPosition(position) {
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  let apiKey = "8b2194a8687d081654f1ef7f23c1a526";
+  let apiCoordsUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`;
+  axios.get(apiCoordsUrl).then(showTemperature);
+}
+function getCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(findPosition);
+}
+let button = document.querySelector("#current-location");
+button.addEventListener("click", getCurrentPosition);
+
+function CurrentDate() {
+  let now = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let CurrentDate = document.querySelector("#CurrentDate");
+  CurrentDate.innerHTML = `${day} ${hours}:${minutes}`;
+}
+
+CurrentDate();
